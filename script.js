07a52ae9a0fd41e3dcaa8058a21fefb7f49c82ce
Hannah Parraga <hannahparraga@email.arizona.edu>
2018-05-27 01:16:50 -0700

    menu outline

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const canny = document.getElementById("canny");
 let ctx = canny.getContext('2d');
+const button = document.getElementById("startButton");
+const menuButton = document.getElementById("menuButton");
+const menu = document.getElementById("menu");
 
 let x = 50;
 let y = 50;
@@ -9,16 +12,40 @@ bun.setAttribute("width", 600);
 bun.setAttribute("height", 600);
 
 function start() {
-	const button = document.getElementById("startButton");
 	button.setAttribute("class", "hide");
+	menuButton.setAttribute("class", "");
 	ctx.drawImage(bun, x, y);
 	
 	document.addEventListener('keypress', evt => {
 		//bun.setAttribute("class", "hide");
 		console.log(evt);
-		ctx.clearRect(x, y, 40, 40);
-		x += 100;
-		y += 100;
-		ctx.drawImage(bun, x, y);
+		if (evt.key === "s") {
+			ctx.clearRect(x, y, 40, 40);
+			y += 10;
+			ctx.drawImage(bun, x, y);
+		} else if (evt.key === "w") {
+			ctx.clearRect(x, y, 40, 40);
+			y -= 10;
+			ctx.drawImage(bun, x, y);
+		} else if (evt.key === "a") {
+			ctx.clearRect(x, y, 40, 40);
+			x -= 10;
+			ctx.drawImage(bun, x, y);
+		} else if (evt.key === "d") {
+			ctx.clearRect(x, y, 40, 40);
+			x += 10;
+			ctx.drawImage(bun, x, y);
+		}
 	});
+}
+
+function openMenu() {
+	menu.setAttribute("class", "");
+	menuButton.setAttribute("class", "hide");
+}
+
+function closeMenu() {
+	menu.setAttribute("class", "hide");
+	menuButton.setAttribute("class", "");
+
 }
